365958588ae95ae744cb5a210b8f45f4acfb531f
Zain Ul Ebad <p166024@nu.edu.pk>
2020-03-08 05:08:21 +0500

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,6 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.metrics import accuracy_score
+import re
