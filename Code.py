116fef45506f180af3c7f57153a2653ca6662093
Zain Ul Ebad <p166024@nu.edu.pk>
2020-03-16 17:25:46 +0500

    Update Code.py

--- Code.py
+++ Code.py
@@ -4,3 +4,15 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.metrics import accuracy_score
 import re
+
+#open dataset
+corpus = open('badges.data').read()
+
+#Removing first line.
+
+res = re.sub(r'','',corpus)
+dlist=res.split('\n')#.remove(dlsit[0])
+dlist.remove(dlist[0])
+dlist.remove(dlist[-1])
+
+#dlist
